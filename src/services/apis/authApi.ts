c53a30689aa1a09e772c66a91f85a95eb2c9dc59
AlessioLami <alessiolami.dev@gmail.com>
2025-08-29 10:59:32 +0200

    test endpoints

--- src/services/apis/authApi.ts
+++ src/services/apis/authApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const authApi = createApi({
     reducerPath: "authApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: "/api/auth",
+        baseUrl: "/auth",
         credentials: "include",
     }),
     endpoints: (builder) => ({
