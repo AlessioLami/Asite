fecc3acb44dbcd3914962a8d0e2705ef6b9f3060
AlessioLami <alessiolami.dev@gmail.com>
2025-08-29 15:18:38 +0200

    Changed baseurl

--- src/services/apis/authApi.ts
+++ src/services/apis/authApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const authApi = createApi({
     reducerPath: "authApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: "/api/auth",
+        baseUrl: "https://asitesens-api.iotalab.app/auth",
         credentials: "include",
     }),
     endpoints: (builder) => ({
