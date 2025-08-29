742d51bb5161ce02a34837a187108beb8cccfeac
AlessioLami <alessiolami.dev@gmail.com>
2025-08-29 11:10:52 +0200

    test 2

--- src/services/apis/authApi.ts
+++ src/services/apis/authApi.ts
@@ -3,26 +3,26 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const authApi = createApi({
     reducerPath: "authApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: "https://asitesens-api.iotalab.app/auth/",
+        baseUrl: "/api/auth",
         credentials: "include",
     }),
     endpoints: (builder) => ({
         login: builder.mutation({
             query: (credentials) => ({
-                url: "login",
+                url: "/login",
                 method: "POST",
                 body: credentials,
             }),
         }),
         logout: builder.mutation({
             query: () => ({
-                url: "logout",
+                url: "/logout",
                 method: "POST",
             }),
         }),
         register: builder.mutation({
             query: (userData) => ({
-                url: "register",
+                url: "/register",
                 method: "POST",
                 body: userData,
             }),
