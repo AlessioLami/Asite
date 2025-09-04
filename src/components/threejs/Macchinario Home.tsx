672d83023fd5fceebcf3ca638872d41769d958a7
AlessioLami <alessiolami.dev@gmail.com>
2025-09-04 10:53:03 +0200

    Fixed build errors

--- src/components/threejs/Macchinario Home.tsx
+++ src/components/threejs/Macchinario Home.tsx
@@ -3,7 +3,6 @@ import Conveyor from "./Conveyor"
 import Conveyor2 from "./Conveyor2"
 import Conveyor3 from "./Conveyor3";
 import Conveyor4 from "./Conveyor4";
-import Conveyor5 from "./Conveyor5";
 import Conveyor6 from "./Conveyor6";
 import Conveyor7 from "./Conveyor7";
 import Tramoggia1 from "./Tramoggia1";
